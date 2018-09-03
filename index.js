cbbdacec9c5c911dc3fd9b36ff68c7eaec43db90
Rogelio Ventura <rogeliovr94@gmail.com>
2018-09-02 21:05:02 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT;
+app.set('view engine', 'ejs');
+app.set('port', port);
+
+app.get('/', function(req, res) {
+	app.use(express.static(path.join(__dirname + '/views')));
+	res.render('pages/index');
+});
+
+app.listen(port, function() {
+	console.log('Client Listening on port' + port);
+})
