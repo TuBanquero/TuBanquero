63223456d6d121353d2845c2f7776fc81b29de70
jekotasek <josekotasek@gmail.com>
2018-12-05 16:50:34 -0600

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TuBanquero' });
+});
+
+router.get('/list', function(req,res,){
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/tubanquero';
+
+  MongoClient.connect(url, function(err,db){
+    if (err){
+      console.log('Unable to connect to the server', err);
+    } else {
+      console.log('Connection established');
+      //var collection = db.collection('users');
+      var collection = db.db().collection('users');
+      collection.find({}).toArray(function(err,result){
+        if(err){
+          res.send(err);
+        } else if(result.length){
+          res.render('userlist', {
+            users : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+        db.close();
+      });
+    }
+  });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: 'Login' });
 });
 
 module.exports = router;
